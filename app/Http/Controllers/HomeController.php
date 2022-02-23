00416070a20dcbffc173efb38b020b5930480170
= <=>
2022-02-23 14:17:37 +0700

    praktikum 2

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index(){
+        echo "Selamat Datang";
+    }
+
+    public function about(){
+        echo "2041720094, Safira Dyah Satiti, TI-2D";
+    }
+
+    public function articles($id){
+        echo "Ini merupakan halaman dengan ID".$id;
+    }
+}
